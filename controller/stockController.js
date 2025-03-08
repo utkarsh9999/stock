b73a8ae8a899e11df445b3fdec317114de696a8b
KazhugaRepo <utkarshtiwari07@gmail.com>
2025-03-08 12:52:06 +0530

    4th commit

--- controller/stockController.js
+++ controller/stockController.js
@@ -1,7 +1,33 @@
 const Stock=require('../models/Stock');
-const {Sequelize}=require('sequelize');
+const Transaction=require('../models/Transaction');
 const app=require('express');
 const router=app.Router();
+const sequelize=require('../config/Sequelize');
+
+
+router.get('/transactions/:stock_id',async (req,resp)=>{
+    try{
+        const stock_id=req.params['stock_id'];
+        const transactions=await Transaction.findAll({
+            where:{
+                stock_id:stock_id
+            }
+        });
+        console.log('\n\n',transactions,'\n\n')
+        resp.status(200).send(transactions);
+    } catch (err){
+
+    }
+});
+
+router.get('/trans-stats',async (req,resp)=>{
+    try {
+        const [stats]=await sequelize.query("SELECT stock_company_name, stock_id , COUNT( stock_id) AS stock_num FROM `transactions` WHERE 1 GROUP BY stock_id");
+        resp.status(200).send(stats);
+    } catch (err){
+
+    }
+});
 
 
 router.get('/stock-list',async (req,resp)=>{
